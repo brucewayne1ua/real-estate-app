a1649a0c9aa791ab8b9335c5e1daf71e675cbfd9
Rustam Ohli <adamsonrustam@gmail.com>
2025-07-14 17:46:48 +0300

    fix profile

--- src/main/java/com/realestate/realestate/controller/ListingController.java
+++ src/main/java/com/realestate/realestate/controller/ListingController.java
@@ -1,19 +1,22 @@
 package com.realestate.realestate.controller;
 
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.realestate.dto.ListingSearchCriteria;
 import com.realestate.realestate.model.Listing;
 import com.realestate.realestate.model.User;
 import com.realestate.realestate.repository.UserRepository;
 import com.realestate.realestate.service.ListingService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
-import java.util.List;              
-import java.util.Optional;
 
 @Controller
 public class ListingController {
@@ -70,4 +73,5 @@ public class ListingController {
         model.addAttribute("criteria", criteria);
         return "search_results";
     }
+
 }
--- src/main/java/com/realestate/realestate/controller/UserController.java
+++ src/main/java/com/realestate/realestate/controller/UserController.java
@@ -1,18 +1,19 @@
 package com.realestate.realestate.controller;
 
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import com.realestate.realestate.model.User;
-import com.realestate.realestate.repository.UserRepository;
-import com.realestate.realestate.repository.ListingRepository;
 import com.realestate.realestate.model.Listing;
-
-import java.util.List;
+import com.realestate.realestate.model.User;
+import com.realestate.realestate.repository.ListingRepository;
+import com.realestate.realestate.repository.UserRepository;
 
 @Controller
 public class UserController {
@@ -39,6 +40,19 @@ public class UserController {
         model.addAttribute("userListings", userListings);
 
         return "profile";
+    }
+
+    @GetMapping("/user_profile/{id}")
+    public String showUserProfile (@PathVariable Long id, Model model) {
+        User user = userRepository.findById(id).orElse(null);
+
+        List<Listing> userListings = listingRepository.findByOwner(user);
+        if (user == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("user", user);
+        model.addAttribute("userListings", userListings);
+        return "user_profile";
     }
 
     @GetMapping("/chats")
